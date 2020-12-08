18b56018c90a41b79fa37e50d4177848fc566fbd
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-08 10:33:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,37 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+
+function titleCase(array) {
+  let a=array.map(element=>element.split(' '));
+  let b=a.map(element=>console.log(element))
+ /* return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');*/
+ return b; 
+}
+
+titleCase("I'm a little tea pot");
+
+
+describe('index.js', function () {
+  describe('titleCased()', function () {
+    it('returns an array with title case tutorial names', function () {
+      expect(titleCased()).to.have.all.members(
+        [
+          "What Does The This Keyword Mean?",
+          "What Is The Contutorialuctor OO Pattern?",
+          "Implementing Blockchain Web API",
+          "The Test Driven Development Workflow",
+          "What Is NaN And How Can We Check For It",
+          "What Is The Difference Between StopPropagation And PreventDefault?",
+          "Immutable State And Pure Functions",
+          "What Is The Difference Between == And ===?",
+          "What Is The Difference Between Event Capturing And Bubbling?",
+          "What Is JSONP?"
+      ]
+      )
+    });
+  });
+});
