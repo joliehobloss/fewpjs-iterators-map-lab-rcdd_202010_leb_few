770a65712694e5aacdf16e0898ca8ecefee5a114
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-08 11:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const titleCased = () => {
 
 
 function ff() {
-  let a="";
-   a=tutorials.map((element)=>{ element.split(' ');
-   let b = a.map((index)=>{
-     return index.charAt(0).toUpperCase() + index.slice(1);
-   }).join(" ")
-    return b;
+  return tutorials.map((element)=>{
+   let arrWord = element.split(' ');
+   let capitalized = arrWord.map((word)=>{
+     return word.charAt(0).toUpperCase() + word.slice(1);
+   })
+    return capitalized.join(" ")
   })
 }
 
